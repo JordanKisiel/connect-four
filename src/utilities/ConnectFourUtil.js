2a2dda89a258a36ebf6d41893c191e94896772e1
Jordan Kisiel <jordan.kisiel@gmail.com>
2023-01-10 16:27:13 -0500

    Completed evaluation function

--- src/utilities/ConnectFourUtil.js
+++ src/utilities/ConnectFourUtil.js
@@ -25,24 +25,221 @@
 
 const testBoard1 = 
 [
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
-    [null, null, null, null, null, null],
+    [true, null, null, null, null, null],
+    [true, null, null, null, null, null],
+    [true, null, null, null, null, null],
+    [true, null, null, null, null, null],
     [null, null, false, null, null, null],
-    [null, null, null, false, null, null],
-    [null, null, null, null, false, null],
-    [null, null, null, null, null, true],
+    [null, false, null, null, null, null],
+    [false, null, null, null, null, null],
 ]
 
 
 
-//console.assert(getLinesOf2(testBoard1, true).length > 0)
-console.log(getNumLinesOf3(testBoard1, false))
+getEvaluation(testBoard1, true)
+
+//###TESTED AND WORKS
+//returns an evaluation score total based upon board position
+function getEvaluation(board, isMaximizingPlayer){
+    //score settings
+    const centerColScore = 4
+    const lineOf2Score = 2
+    const lineOf3Score = 5
+    const winScore = 100000 //arbitrarily large to make other scoring irrelevant
+    const oppLineOf2Score = -2
+    const oppLineOf3Score = -100
+
+    let evalScore = 0
+
+    //Win?
+    const winningSlots = getWinningSlots(board)
+    //use indices from winning slots
+    const winningPlayer = winningSlots.length > 0 ? board[winningSlots[0][0]][winningSlots[0][1]] : null
+    //update score and return immediately (no need to do anymore evaluation)
+    if(winningSlots.length !== 0 && winningPlayer === isMaximizingPlayer){
+        console.log('Win!')
+        evalScore += winScore
+        return evalScore
+    }
+
+    //check center column for player discs
+    const discsInCenter = board[3].reduce((occurences, curr) => {
+        return curr === isMaximizingPlayer ? occurences + 1 : occurences + 0
+    }, 0)
+
+    //discs in center column?
+    console.log('Discs in center: ', discsInCenter)
+    evalScore += discsInCenter * centerColScore
+
+    //lines of 2?
+    console.log('Lines of 2: ', getNumLinesOf2(board, isMaximizingPlayer))
+    evalScore += getNumLinesOf2(board, isMaximizingPlayer) * lineOf2Score
+
+    //lines of 3?
+    console.log('Lines of 3: ', getNumLinesOf3(board, isMaximizingPlayer))
+    evalScore += getNumLinesOf3(board, isMaximizingPlayer) * lineOf3Score
+
+    //opp line of two?
+    console.log('Opp Lines of 2: ', getNumLinesOf2(board, !isMaximizingPlayer))
+    evalScore += getNumLinesOf2(board, !isMaximizingPlayer) * oppLineOf2Score
+
+    //opp line of three?
+    console.log('Opp Lines of 3: ', getNumLinesOf3(board, !isMaximizingPlayer))
+    evalScore += getNumLinesOf3(board, !isMaximizingPlayer) * oppLineOf3Score
+
+    //return final eval score
+    console.log('Final score: ', evalScore)
+    return evalScore
+}
+
+//return best column to drop disc in
+function getBestMove(board){
+    let bestValue = Number.NEGATIVE_INFINITY
+    let bestMove = undefined
+
+    //for each column, run the minimax function and return col with optimum value
+    //only test columns that have an empty space still available
+}
+
+//function returns max score it can find assuming other player is also
+//playing perfectly
+function minimax(board, depth, isMaximizingPlayer){
+    //base case
+    const gameOver = (getWinningSlots(board).length !== 0 || isBoardFull(board))
+    if(depth === 0 || gameOver){
+        return evaluation(board, isMaximizingPlayer)
+    }
+
+    if(isMaximizingPlayer){
+        let maxEval = Number.NEGATIVE_INFINITY
+        getChildPositions(board).forEach((childPos) => {
+            let currEval = minimax(childPos, depth - 1, !isMaximizingPlayer)
+            maxEval = Math.max(maxEval, currEval)
+        })
+        return maxEval
+    }
+    else{
+        let minEval = Number.POSITIVE_INFINITY
+        getChildPositions(board).forEach((childPos) => {
+            let currEval = minimax(childPos, depth - 1, !isMaximizingPlayer)
+            minEval = Math.min(minEval, currEval)
+        })
+        return minEval
+    }
+}
+
+//###TESTED AND WORKS
+//returns winning slots (spaces) but can also be used
+//to test if there's a winner at all (array length will be 0 if no winner)
+function getWinningSlots(board){
+    const height = board[0].length
+    const width = board.length
+    const emptySlot = null
+
+    for(let row = 0; row < width; row++){ 
+        for(let col = 0; col < height; col++){
+            let slot = board[row][col]
+
+            if(slot === emptySlot){
+
+                continue //don't check starting from empty slots
+            }
+
+            if(col + 3 < height &&
+                slot === board[row][col + 1] && //look right
+                slot === board[row][col + 2] &&
+                slot === board[row][col + 3]){
+                    return [[row, col],
+                            [row, col + 1], 
+                            [row, col + 2], 
+                            [row, col + 3]]
+            }
+
+            if(row + 3 < width){
+                if(slot === board[row + 1][col] && //look up
+                   slot === board[row + 2][col] &&
+                   slot === board[row + 3][col]){
+                        return [[row, col], 
+                                [row + 1, col], 
+                                [row + 2, col], 
+                                [row + 3, col]]
+                }
+
+                if(col + 3 < height &&
+                    slot === board[row + 1][col + 1] && //look up & right
+                    slot === board[row + 2][col + 2] &&
+                    slot === board[row + 3][col + 3]){
+                        return [[row, col],
+                                [row + 1, col + 1], 
+                                [row + 2, col + 2], 
+                                [row + 3, col + 3]]
+                }
+
+                if(col - 3 >= 0 &&
+                    slot === board[row + 1][col - 1] && //look up & left
+                    slot === board[row + 2][col - 2] &&
+                    slot === board[row + 3][col - 3]){
+                        return [[row, col],
+                                [row + 1, col - 1], 
+                                [row + 2, col - 2], 
+                                [row + 3, col - 3]]
+                }
+            }
+        }
+    }
+
+    return [] //if no winning line, return no winning slots
+}
+
+//##TESTED AND WORKS
+//check every space and return true only if there are no empty spaces
+function isBoardFull(board){
+    for(let i = 0; i < board.length; i++){
+        for(let j = 0; j < board[0].length; j++){
+            if(board[i][j] === null){
+                return false
+            }
+        }
+    }
+
+    return true
+}
+
+//##TESTED AND WORKS
+//returns every single possible next board from the current board position
+function getChildPositions(board, isMaximizingPlayer){
+    let childPositions = []
+
+    //for each column
+    for(let i = 0; i < board.length; i++){
+        //if column isn't full
+        if(board[i][board[i].length - 1] === null){
+            //update board with disc in that column
+            const childBoard = board.map((col, index) => {
+                //null testing function
+                const isNull = (element) => element === null
+                const firstNullIndex = col.findIndex(isNull)
+
+                return index === i ?
+                            col.map((space, index) => {
+                                return index === firstNullIndex ? isMaximizingPlayer : space
+                            })
+                            :col
+            })
+            
+            //push that new board into childPositions
+            childPositions.push(childBoard)
+        }
+    }
+    //return array of child positions
+    return childPositions
+}
 
 //###TESTED AND WORKS
 //returns the number of lines of 2 (used for position evaluation)
 //todo: it's possible that I made this function more complex than it needs to be
 //review at a future date
+//canMakeWinningLine function also has side effect (duplicatedLinesCount)
 function getNumLinesOf2(board, player){
     let linesOf2 = []
     const width = board.length
@@ -241,6 +438,7 @@ function getNumLinesOf3(board, player){
     let linesOf3 = []
     const width = board.length
     const height = board[0].length
+    let duplicatedLinesCount = 0
 
     //testing functions
     const isExactly3PlayerDiscs = (line) => {
@@ -261,6 +459,11 @@ function getNumLinesOf3(board, player){
         return isExactly3PlayerDiscs(line) && isEmptySpaceInLine(line)
     }
 
+    //looks for two specific patterns that get double counted and need to be only single counted
+    const isDuplicatedLine = (line) => {
+        return (line[1] === null || line[2] === null)
+    }
+
     for(let row = 0; row < width; row++){
         for(let col = 0; col < height; col++){
             let slot = board[row][col]
@@ -277,6 +480,7 @@ function getNumLinesOf3(board, player){
                     
                 if(isLineOf3(lineToCheck)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look up
@@ -290,6 +494,7 @@ function getNumLinesOf3(board, player){
           
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look left
@@ -303,6 +508,7 @@ function getNumLinesOf3(board, player){
                     
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look down
@@ -316,6 +522,7 @@ function getNumLinesOf3(board, player){
                     
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look up & right
@@ -329,6 +536,7 @@ function getNumLinesOf3(board, player){
 
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look up & left
@@ -342,6 +550,7 @@ function getNumLinesOf3(board, player){
 
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look down & left
@@ -355,6 +564,7 @@ function getNumLinesOf3(board, player){
 
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
 
                 //look down & right
@@ -368,196 +578,11 @@ function getNumLinesOf3(board, player){
 
                 if(isLineOf3(lineToCheck, contextSlots)){
                     linesOf3.push(lineToCheck)
+                    duplicatedLinesCount = isDuplicatedLine(lineToCheck) ? duplicatedLinesCount + 1 : duplicatedLinesCount
                 }
             }
         }
     }
 
-
-    return linesOf3.length
+    return linesOf3.length - (duplicatedLinesCount / 2)
 }
-
-//return best column to drop disc in
-function getBestMove(board){
-    let bestValue = Number.NEGATIVE_INFINITY
-    let bestMove = undefined
-
-    //for each column, run the minimax function and return col with optimum value
-    //only test columns that have an empty space still available
-}
-
-//function returns max score it can find assuming other player is also
-//playing perfectly
-function minimax(board, depth, isMaximizingPlayer){
-    //base case
-    const gameOver = (getWinningSlots(board).length !== 0 || isBoardFull(board))
-    if(depth === 0 || gameOver){
-        return evaluation(board, isMaximizingPlayer)
-    }
-
-    if(isMaximizingPlayer){
-        let maxEval = Number.NEGATIVE_INFINITY
-        getChildPositions(board).forEach((childPos) => {
-            let currEval = minimax(childPos, depth - 1, !isMaximizingPlayer)
-            maxEval = Math.max(maxEval, currEval)
-        })
-        return maxEval
-    }
-    else{
-        let minEval = Number.POSITIVE_INFINITY
-        getChildPositions(board).forEach((childPos) => {
-            let currEval = minimax(childPos, depth - 1, !isMaximizingPlayer)
-            minEval = Math.min(minEval, currEval)
-        })
-        return minEval
-    }
-}
-
-//###TESTED AND WORKS
-//returns winning slots (spaces) but can also be used
-//to test if there's a winner at all (array length will be 0 if no winner)
-function getWinningSlots(board){
-    const height = board[0].length
-    const width = board.length
-    const emptySlot = null
-
-    for(let row = 0; row < width; row++){ 
-        for(let col = 0; col < height; col++){
-            let slot = board[row][col]
-
-            if(slot === emptySlot){
-
-                continue //don't check starting from empty slots
-            }
-
-            if(col + 3 < height &&
-                slot === board[row][col + 1] && //look right
-                slot === board[row][col + 2] &&
-                slot === board[row][col + 3]){
-                    return [[row, col],
-                            [row, col + 1], 
-                            [row, col + 2], 
-                            [row, col + 3]]
-            }
-
-            if(row + 3 < width){
-                if(slot === board[row + 1][col] && //look up
-                   slot === board[row + 2][col] &&
-                   slot === board[row + 3][col]){
-                        return [[row, col], 
-                                [row + 1, col], 
-                                [row + 2, col], 
-                                [row + 3, col]]
-                }
-
-                if(col + 3 < height &&
-                    slot === board[row + 1][col + 1] && //look up & right
-                    slot === board[row + 2][col + 2] &&
-                    slot === board[row + 3][col + 3]){
-                        return [[row, col],
-                                [row + 1, col + 1], 
-                                [row + 2, col + 2], 
-                                [row + 3, col + 3]]
-                }
-
-                if(col - 3 >= 0 &&
-                    slot === board[row + 1][col - 1] && //look up & left
-                    slot === board[row + 2][col - 2] &&
-                    slot === board[row + 3][col - 3]){
-                        return [[row, col],
-                                [row + 1, col - 1], 
-                                [row + 2, col - 2], 
-                                [row + 3, col - 3]]
-                }
-            }
-        }
-    }
-
-    return [] //if no winning line, return no winning slots
-}
-
-
-//##TESTED AND WORKS
-//check every space and return true only if there are no empty spaces
-function isBoardFull(board){
-    for(let i = 0; i < board.length; i++){
-        for(let j = 0; j < board[0].length; j++){
-            if(board[i][j] === null){
-                return false
-            }
-        }
-    }
-
-    return true
-}
-
-//##TESTED AND WORKS
-//returns every single possible next board from the current board position
-function getChildPositions(board, isMaximizingPlayer){
-    let childPositions = []
-
-    //for each column
-    for(let i = 0; i < board.length; i++){
-        //if column isn't full
-        if(board[i][board[i].length - 1] === null){
-            //update board with disc in that column
-            const childBoard = board.map((col, index) => {
-                //null testing function
-                const isNull = (element) => element === null
-                const firstNullIndex = col.findIndex(isNull)
-
-                return index === i ?
-                            col.map((space, index) => {
-                                return index === firstNullIndex ? isMaximizingPlayer : space
-                            })
-                            :col
-            })
-            
-            //push that new board into childPositions
-            childPositions.push(childBoard)
-        }
-    }
-    //return array of child positions
-    return childPositions
-}
-
-//returns an evaluation score total based upon board position
-function evaluation(currentBoard, isMaximizingPlayer){
-    //score settings
-    const centerColScore = 4
-    const lineOf2Score = 2
-    const lineOf3Score = 5
-    const winScore = 10000
-    const oppLineOf2Score = -2
-    const oppLineOf3Score = -100
-
-    let evalScore = 0
-
-    const discsInCenter = currentBoard[3].reduce((occurences, curr) => {
-        return curr === isMaximizingPlayer ? 1 : 0
-    })
-
-    evalScore += discsInCenter * centerColScore
-
-    //lines of 2? +2 for each of them
-    evalScore += getLinesOfN(board, 2, isMaximizingPlayer).length * lineOf2Score
-
-    //lines of 3? +5 for each of them
-    evalScore += getLinesOfN(board, 3, isMaximizingPlayer).length * lineOf3Score
-
-    //Win? +10000
-    const winningSlots = getWinningSlots(currentBoard)
-    if(winningSlots.length !== 0 && winningSlots[0][0] === isMaximizingPlayer){
-        evalScore += winScore
-    }
-
-    //opp line of two? -2 for each
-    evalScore += getLinesOfN(board, 2, !isMaximizingPlayer).length * oppLineOf2Score
-
-    //opp line of three? -100 for each
-    evalScore += getLinesOfN(board, 3, !isMaximizingPlayer).length * oppLineOf3Score
-
-    //return final eval score
-    return evalScore
-}
-
